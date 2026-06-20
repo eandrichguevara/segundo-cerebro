d7a8c9d2ee2c2403d036065605e6e1c071243ba7
eandrichguevara <eandrichguevara@gmail.com>
2026-06-20 17:03:02 -0400

    build: apply Kotlin Android plugin and remove redundant kotlinOptions configuration

--- appmovil/android/app/build.gradle.kts
+++ appmovil/android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
@@ -14,10 +15,6 @@ android {
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
